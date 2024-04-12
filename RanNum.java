0dff0bb3a0c02c0a46f48d1ff47dc2bc1a46c359
Josue <carlosjosuelopezsolano98@gmail.com>
2024-04-11 18:39:09 -0600

    before I do detach and fetch

--- RanNum.java
+++ RanNum.java
@@ -7,10 +7,6 @@ class RandomNum {
     }
 
     public int getNum(){
-<<<<<<< HEAD
-        return ran.nextInt(100) + new Numerito().getNum() +10000;    
-=======
-        return ran.nextInt(100) + new Numerito().getNum()+2*2+2;    
->>>>>>> 737637743e96ec361acce24b09b4af7fe097874b
+        return ran.nextInt(100) + new Numerito().getNum()+2*2+21;    
     }
 }
