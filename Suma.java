a5996f578a29623515d3bbbc0f059a7732b1bf77
Josue <carlosjosuelopezsolano98@gmail.com>
2024-04-11 19:22:47 -0600

    hjhh

--- Suma.java
+++ Suma.java
@@ -1,5 +1,6 @@
 class SumaNum {
     public void sumaFinal(int num1,int num2){
-        System.out.println(" The sum is " + (num1+num2) );
+        System.out.println("Coflicts pull ");
+        System.out.println(" The sum is " + (num1+num2) + " final " );
     }
 }
