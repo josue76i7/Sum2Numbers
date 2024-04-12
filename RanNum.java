3a8cc6f2f7188db678dd49a9f4934bb99c78c2c8
josue76i7 <140025175+josue76i7@users.noreply.github.com>
2024-04-11 19:29:29 -0600

    Update RanNum.java

--- RanNum.java
+++ RanNum.java
@@ -7,10 +7,6 @@ class RandomNum {
     }
 
     public int getNum(){
-<<<<<<< HEAD
-        return ran.nextInt(100) + new Numerito().getNum()+2*2+21;    
-=======
-        return ran.nextInt(100) + new Numerito().getNum()+1*11+11;    
->>>>>>> 20d2f88999d7da7269f02046abf5f5ff2ebb46ef
+        return ran.nextInt(100) + new Numerito().getNum()+1111;    
     }
 }
