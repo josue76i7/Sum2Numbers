26d87ae9ba4d5fc4d17b324f981378bd0ef1593e
josue76i7 <140025175+josue76i7@users.noreply.github.com>
2024-04-13 19:50:39 -0600

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,12 +1,11 @@
 class Menu {
    static RandomNum ran;
    static SumaNum sum;
-
+   int a = 100;
     public static void main(String[] args) {
         ran = new RandomNum();
         sum = new SumaNum();
         sum.sumaFinal(ran.getNum(), ran.getNum());
-        System.out.println("Good bay");
     }
 }    
 
