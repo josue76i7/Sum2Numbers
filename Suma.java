51ddfdba4c1ed17971b643de89115408e38500a4
josue76i7 <140025175+josue76i7@users.noreply.github.com>
2024-04-24 19:55:30 -0600

    Update Suma.java

--- Suma.java
+++ Suma.java
@@ -1,5 +1,12 @@
 class SumaNum {
     public void sumaFinal(int num1,int num2){
+
+         System.out.println((num1+num2) );
+         System.out.println((num1+num2) );
+         System.out.println((num1+num2) );}
+     System.out.println((num1+num2) );
+ System.out.println((num1+num2) );
+ System.out.println((num1+num2) );
         System.out.println((num1+num2) );
     }
 }
