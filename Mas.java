b97e8bb1355926038278feef7f31ca98bf100020
josue76i7 <140025175+josue76i7@users.noreply.github.com>
2024-04-24 19:54:56 -0600

    Update Mas.java

--- Mas.java
+++ Mas.java
@@ -1,7 +1,12 @@
 class Numerito{
     
-    public int getNum() {
-        return 5555;
+    public in0t getNum() {
+        int a = 0;
+        int b = 0;
+        int f = 0; 
+        int c = 0; 
+        int d = 0;
+        return 81+82;
     }
 }
 
