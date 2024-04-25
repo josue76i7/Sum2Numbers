d944102845b0a53681d42708243f050f6a113533
josue76i7 <140025175+josue76i7@users.noreply.github.com>
2024-04-24 18:42:00 -0600

    tHE LAS MESSS java

--- Menu.java
+++ Menu.java
@@ -3,13 +3,11 @@ class Menu {
    static SumaNum sum;
    int a = 100;
     public static void main(String[] args) {
-        ran = new RandomNum();
+       
+       ran = new RandomNum();
         sum = new SumaNum();
         sum.sumaFinal(ran.getNum(), ran.getNum());
-<<<<<<< HEAD
-        System.out.println("No");
-=======
->>>>>>> 26d87ae9ba4d5fc4d17b324f981378bd0ef1593e
+       
     }
 }    
 
